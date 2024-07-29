f3d9aa96a2e0f3a160579ffcc07dcb9c0aa4076b
PR <161607150+cmd-codee@users.noreply.github.com>
2024-07-29 17:55:41 +0300

    Add files via upload

---
+++ Мед-программа.py
@@ -0,0 +1,25 @@
+print(
+    "Привет,напиши имя, фамилию, отчество и дата рождения для записи в мед-кабинет."
+)
+a = input("Нажмите ENTER для продолжения")
+name = []
+surname = []
+patronymic = []
+date_of_birth = []
+name.append(input("Имя:"))
+surname.append(input("Фамилия:"))
+patronymic.append(input("Отчество:"))
+date_of_birth.append(input("Дата рождения:"))
+print(
+    "Спасибо,нужен ли вывод информации? Если да напишите Да, Если нет напишите любое слово"
+)
+b = input()
+if b == "Да":
+  print(name[0]), print(surname[0]), print(patronymic[0]), print(
+      date_of_birth[0])
+if b != "Да":
+  print("Спасибо")
+input(
+    "Нажмите любую клавишу для выхода"
+)  #Данная строка ни на что не влияет, ни к чему не присоединена и не связана,нужна если вы открываете программу в Microsoft Terminale
+exit(0)
